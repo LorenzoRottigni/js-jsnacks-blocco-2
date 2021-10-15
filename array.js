eb27073754d3e044b36cab167467a80eb9526bd1
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-15 17:03:29 +0200

    commenti

--- array.js
+++ array.js
@@ -6,15 +6,7 @@ const array2 = [5];
 const array1 = ['ciao'];
 const array2 = ['hola', 'hello'];
 */
-//while the index of array 1 is smaller then array2 one
-while(array2.length<array1.length){
-    //generate random int
-    const randN = Math.random()
-    //add random int
-    array2.push(randN);
-    //console log new element
-    console.log('new element inserted: ' + randN)
-}
+//while the index of array 1 is smaller then array 2 one
 while(array1.length<array2.length){
     //generate random int
     const randN = Math.random()
@@ -23,5 +15,15 @@ while(array1.length<array2.length){
     //console log new element
     console.log('new element inserted: ' + randN)
 }
+//while the index of array 2 is smaller then array 1 one
+while(array2.length<array1.length){
+    //generate random int
+    const randN = Math.random()
+    //add random int
+    array2.push(randN);
+    //console log new element
+    console.log('new element inserted: ' + randN)
+}
+
 //now arrays have equal size
 console.log('arrays now have the same size')
